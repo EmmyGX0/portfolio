38cef46905fa70c4fe1299d2a09b42a01347c17b
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-04-30 00:21:16 -0500

    Add data.ts file for storing navigation links

---
+++ lib/data.ts
@@ -0,0 +1,22 @@
+export const links = [  
+  {
+    name: 'Experience',
+    hash: '#experience'
+  },
+  {
+    name: 'Projects',
+    hash: '#projects'
+  },
+  {
+      name: 'Skills',
+      hash: '#skills'
+  },
+  {
+      name: 'About',
+      hash: '#about'
+  },
+  {
+      name: 'Contact',
+      hash: '#contact'
+  }
+]
