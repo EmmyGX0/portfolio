6fee11a3675dcc5281725e9ade2e988ba0212fbe
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-04-30 00:20:38 -0500

    Add Next.js configuration for remote images and update package dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.githubassets.com',
+        pathname: '/images/modules/site/home-campaign/hero-bg.webp',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.githubassets.com',
+        pathname: '/images/modules/site/issues/illo/issues-plan.png',
+        port: '',
+      }
+    ]
+  }
+};
 
 export default nextConfig;
