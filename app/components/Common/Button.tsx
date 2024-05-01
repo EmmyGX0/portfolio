c353cb53d693ca68416c77759b76fed087647463
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-05-01 17:33:55 -0500

    Update Button component with new props and functionality

--- app/components/Common/Button.tsx
+++ app/components/Common/Button.tsx
@@ -1,10 +1,22 @@
 type Props = {
   text: string
+  type?: string
+  url?: string
 }
 
-const Button = ({text}: Props) => {
+const Button = ({text, type, url}: Props) => {
+  const handleClick = () => {
+    if (type === 'contact') {
+      window.open("mailto:emmanuel.lifeg@outlook.com", '_blank')
+      return
+    }
+    window.open(url, '_blank')
+  }
   return (
-    <div className="rounded-lg w-fit flex justify-center items-center h-10 bg-brand text-brand-text font-semibold px-3 py-1 cursor-pointer hover:bg-brand/25 duration-300 transition-all">
+    <div 
+      className="rounded-lg w-fit text-sm sm:text-base flex justify-center items-center h-10 bg-brand text-brand-text font-semibold px-3 py-1 cursor-pointer hover:bg-brand/25 duration-300 transition-all"
+      onClick={() => handleClick()}  
+    >
       {text}
     </div>
   )
