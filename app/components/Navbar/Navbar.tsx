8ac9a7abe06cff3e221240d141177338387c5030
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-04-30 20:41:26 -0500

    Update Navbar component with new styles and functionality

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,10 +1,24 @@
+'use client'
 import { links } from "@/lib/data"
+import { useMotionValueEvent, useScroll } from "framer-motion"
 
 const Navbar = () => {
+
+  const { scrollYProgress } = useScroll()
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log(latest > 0.3125 ? 'true' : 'false')
+  })
+
   return (
-    <nav className="w-full flex justify-evenly py-8 text-base">
+    <nav className="bg-black sidebar w-16">
       {links.map(link => (
-        <a key={link.name} href={link.hash} className="text-white tracking-widest font-thin hover:font-normal transition-all duration-200">
+        <a
+          key={link.name}
+          href={link.hash}
+          className={`text-white sidebar_link tracking-widest font-thin hover:bg-slate-400/25 transition-all duration-200`}
+          // className={`${router.pathname === link.hash ? 'bg-slate-400/25 border-t-brand' : ''} text-white sidebar_link tracking-widest font-thin hover:bg-slate-400/25 transition-all duration-200`}
+        >
           {link.name}
         </a>
       ))}
