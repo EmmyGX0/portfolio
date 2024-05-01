79858227adcd7f768484c0a6a38f1f60772c3a51
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-05-01 17:33:49 -0500

    Update About component with responsive styles and font size adjustments

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -3,16 +3,16 @@ import TextTransition from '../Common/TextTransition'
 
 const About = () => {
   return (
-    <div id="about" className="flex flex-col w-full pb-20">
+    <div id="about" className="flex flex-col w-full pb-20 section">
       <div className="w-full overflow-hidden flex items-center gap-4">
         <div className='relative'>
           <TextTransition>
-            <h1 className="text-brand-text font-bold text-5xl">About<span className="text-brand">.</span></h1>
+            <h1 className="text-brand-text font-bold text-4xl md:text-5xl">About<span className="text-brand">.</span></h1>
           </TextTransition>
         </div>
         <figure className="w-full h-[1px] bg-white opacity-30" />
       </div>
-      <div className='grid grid-cols-1 gap-6 md2:grid-cols-[2fr_1fr] mt-5 text-brand-sub text-xl font-light'>
+      <div className='grid grid-cols-1 gap-6 mt-5 text-brand-sub text-lg md:text-xl font-light'>
         <div className='flex gap-2 flex-col'>
           <div className='relative overflow-hidden'>
             <TextTransition className='flex items-center gap-4'>
@@ -36,16 +36,16 @@ const About = () => {
               (Fallout fan here), watch Basketball, MMA, and my favorite sport is Boxing.
             </TextTransition>
           </div>
-          <div className='flex flex-col mt-5 gap-2 relative'>
+          <div className='flex flex-col mt-5 gap-2 relative overflow-hidden'>
             <TextTransition className='flex flex-col gap-4'>
               <div className='flex gap-5 items-center'>
                 <span className='bg-brand py-1 text-black font-bold flex justify-center items-center text-md w-8 rounded-lg'>{`>_`}</span>
-                <label className='text-brand-text text-2xl font-bold'>Technologies I use</label>
+                <label className='text-brand-text text-xl md:text-2xl font-bold'>Technologies I use</label>
               </div>
               <div className='flex flex-wrap gap-2'>
                 {technologies.map(tech => (
                   <div key={tech.name} className='flex justify-center items-center bg-brand-sidebarBg rounded-xl px-2 py-1'>
-                    <label className='text-brand-text text-sm tracking-wide'>{tech.name}</label>
+                    <label className='text-brand-text md:text-sm text-xs tracking-wide'>{tech.name}</label>
                   </div>
                 ))}
               </div>
