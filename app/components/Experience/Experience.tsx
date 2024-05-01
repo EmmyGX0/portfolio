a6fc9c91a705f04d1b9c06fcb93978c474d08292
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-04-30 20:41:18 -0500

    Update Experience component with new design and content

--- app/components/Experience/Experience.tsx
+++ app/components/Experience/Experience.tsx
@@ -1,33 +1,70 @@
 'use client'
-import { PiBriefcaseLight } from 'react-icons/pi'
-import ExperienceCard from './ExperienceCard'
+import TextTransition from '../Common/TextTransition'
 import { motion } from 'framer-motion'
 
 const Experience = () => {
   return (
-    <div>
-      <div className="w-full pt-56 items-center flex gap-10 pb-20">
-        <div className="flex justify-center text-brand-text items-center p-1 relative">
-          <motion.span 
-            className="rounded-full bg-blue-600/25 p-2"
-            initial={{ opacity: 0, y: -10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1.7 }}
-          >
-            <PiBriefcaseLight size={30} />
-          </motion.span>
+    <motion.div id="experience" className="flex flex-col w-full pb-20">
+      <div className="w-full overflow-hidden flex items-center gap-4">
+        <figure className="w-full h-[1px] bg-white opacity-30" />
+        <div className='relative'>
+          <TextTransition>
+            <h1 className="text-brand-text font-bold text-5xl">Experience<span className="text-brand">.</span></h1>
+          </TextTransition>
         </div>
-        <motion.div 
-          className="flex flex-col gap-5"
-          initial={{ opacity: 0, x: -10 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, delay: 1.7 }}
-        >
-          <span className="text-brand-text text-2xl font-light tracking-wider">Work experience</span>
-        </motion.div>
       </div>
-        <ExperienceCard />
-    </div>
+      <div className='flex flex-col mt-5'>
+        <div className='flex justify-between flex-wrap'>
+          <h2 className='text-brand-text relative font-bold text-xl'>
+            <TextTransition>
+              We Book You
+            </TextTransition>
+          </h2>
+          <label className='text-brand-sub relative text-lg'>
+            <TextTransition>
+              May 2022 - Present
+            </TextTransition>
+          </label>
+        </div>
+        <div className='flex justify-between mt-2 flex-wrap'>
+          <label className='text-brand relative font-bold text-lg'>
+            <TextTransition>
+              Frontend Developer
+            </TextTransition>
+          </label>
+          <label className='text-brand-sub relative text-lg'>
+            <TextTransition>
+              Remote
+            </TextTransition>
+          </label>
+        </div>
+      </div>
+
+      <div className='text-brand-sub text-xl mt-2'>
+        <div className='relative'>
+          <TextTransition>
+            As a front-end developer, my role is to enhance the user experience through the implementation of new features and the improvement of components.
+          </TextTransition>
+        </div>
+      <ul className="mt-2 flex flex-col gap-1">
+        <li className='relative'>
+          <TextTransition>
+            <span className="text-2xl">•</span> I collaborate closely with a multidisciplinary team, including designers (layout) and backend developers (API consumption) for efficient integration using React and NextJS in the development of modern web applications.
+          </TextTransition>
+        </li>
+        <li className='relative'>
+          <TextTransition>
+            <span className="text-2xl">•</span> I contribute to maintaining a responsive design by utilizing TailwindCSS to ensure a consistent experience across all devices.
+          </TextTransition>
+        </li>
+        <li className='relative'>
+          <TextTransition>
+          <span className="text-2xl">•</span> Redux and Zustand are employed for state management.
+          </TextTransition>
+        </li>
+      </ul>
+      </div>
+    </motion.div>
   )
 }
 
