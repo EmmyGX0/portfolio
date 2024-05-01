335122b3c2419ce628073f5ed90614665a5c8862
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-04-30 20:41:00 -0500

    Update page layout and components

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,20 @@
 import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
-import { delay, motion } from 'framer-motion';
+import Projects from "./components/Projects/Projects";
+import About from "./components/About/About";
 
 export default function Home() {
-  const variants = {
-    hidden: {
-      height: 0
-    },
-    visible: {
-      height: 270,
-      transition: {
-        duration: 0.8,
-      }
-    }
-  }
-  
+
   return (
-    <div className='bg-brand-background flex flex-col items-center w-full pb-20 h-full min-h-screen'>
-      <div className="max-w-5xl w-full px-5 relative">
-        <Navbar />
-        <Hero />
-        <motion.figure
-          className="w-[4px] bg-gradient-to-b from-brand/25 to-blue-600/25 absolute left-[45px] top-[410px]"
-          variants={variants}
-          initial='hidden'
-          whileInView='visible'
-          viewport={{ once: true, margin: '-100px' }}
-        />
-        <div className="max-w-4xl">
+    <div className='bg-brand-background flex'>
+      <Navbar />
+      <div className="w-full flex justify-center flex-col items-center">
+        <div className="max-w-5xl w-full flex flex-col px-10">
+          <Hero />
+          <About />
           <Experience />
+          <Projects />
         </div>
       </div>
     </div>
