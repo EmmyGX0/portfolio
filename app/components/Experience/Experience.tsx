4a8c1430e3e88e4d928d146863e493d9d1dd6024
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-05-01 17:34:06 -0500

    Update Experience component with responsive styles and font size adjustments

--- app/components/Experience/Experience.tsx
+++ app/components/Experience/Experience.tsx
@@ -4,35 +4,35 @@ import { motion } from 'framer-motion'
 
 const Experience = () => {
   return (
-    <motion.div id="experience" className="flex flex-col w-full pb-20">
+    <motion.div id="experience" className="flex flex-col w-full pb-20 section">
       <div className="w-full overflow-hidden flex items-center gap-4">
         <figure className="w-full h-[1px] bg-white opacity-30" />
-        <div className='relative'>
+        <div>
           <TextTransition>
-            <h1 className="text-brand-text font-bold text-5xl">Experience<span className="text-brand">.</span></h1>
+            <h1 className="text-brand-text font-bold text-4xl md:text-5xl">Experience<span className="text-brand">.</span></h1>
           </TextTransition>
         </div>
       </div>
       <div className='flex flex-col mt-5'>
         <div className='flex justify-between flex-wrap'>
-          <h2 className='text-brand-text relative font-bold text-xl'>
+          <h2 className='text-brand-text font-bold text-lg md:text-xl'>
             <TextTransition>
               We Book You
             </TextTransition>
           </h2>
-          <label className='text-brand-sub relative text-lg'>
+          <label className='text-brand-sub text-base md:text-lg'>
             <TextTransition>
               May 2022 - Present
             </TextTransition>
           </label>
         </div>
         <div className='flex justify-between mt-2 flex-wrap'>
-          <label className='text-brand relative font-bold text-lg'>
+          <label className='text-brand  font-bold text-base md:text-lg'>
             <TextTransition>
               Frontend Developer
             </TextTransition>
           </label>
-          <label className='text-brand-sub relative text-lg'>
+          <label className='text-brand-sub  text-base md:text-lg'>
             <TextTransition>
               Remote
             </TextTransition>
@@ -40,26 +40,26 @@ const Experience = () => {
         </div>
       </div>
 
-      <div className='text-brand-sub text-xl mt-2'>
-        <div className='relative'>
+      <div className='text-brand-sub text-lg md:text-xl mt-2'>
+        <div>
           <TextTransition>
             As a front-end developer, my role is to enhance the user experience through the implementation of new features and the improvement of components.
           </TextTransition>
         </div>
       <ul className="mt-2 flex flex-col gap-1">
-        <li className='relative'>
+        <li>
           <TextTransition>
-            <span className="text-2xl">•</span> I collaborate closely with a multidisciplinary team, including designers (layout) and backend developers (API consumption) for efficient integration using React and NextJS in the development of modern web applications.
+            <span>•</span> I collaborate closely with a multidisciplinary team, including designers (layout) and backend developers (API consumption) for efficient integration using React and NextJS in the development of modern web applications.
           </TextTransition>
         </li>
-        <li className='relative'>
+        <li>
           <TextTransition>
-            <span className="text-2xl">•</span> I contribute to maintaining a responsive design by utilizing TailwindCSS to ensure a consistent experience across all devices.
+            <span>•</span> I contribute to maintaining a responsive design by utilizing TailwindCSS to ensure a consistent experience across all devices.
           </TextTransition>
         </li>
-        <li className='relative'>
+        <li>
           <TextTransition>
-          <span className="text-2xl">•</span> Redux and Zustand are employed for state management.
+          <span>•</span> Redux and Zustand are employed for state management.
           </TextTransition>
         </li>
       </ul>
