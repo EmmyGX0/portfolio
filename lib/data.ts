f5e46629153625068fabc29fffbe66dd888f3272
EmmyGX0 <emmanuel.lifeg@outlook.com>
2024-04-30 20:41:32 -0500

    Update data.ts with new links, variants, and technologies

--- lib/data.ts
+++ lib/data.ts
@@ -1,22 +1,103 @@
 export const links = [  
   {
-    name: 'Experience',
+      name: 'About',
+      hash: '#about'
+  },
+  {
+    name: 'Exp.',
     hash: '#experience'
   },
   {
     name: 'Projects',
     hash: '#projects'
   },
-  {
-      name: 'Skills',
-      hash: '#skills'
-  },
-  {
-      name: 'About',
-      hash: '#about'
-  },
+  // {
+  //     name: 'Skills',
+  //     hash: '#skills'
+  // },
   {
       name: 'Contact',
       hash: '#contact'
   }
+]
+
+export const variantsText = {
+  hidden: {
+    y: 50,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5, 
+      delay: 0.5
+    }
+  }
+}
+
+export const variantsFigure = {
+  hidden: {
+    width: 'full'
+  },
+  visible: {
+    width: '0px',
+    transition: {
+      duration: 0.5
+    }
+  }
+}
+
+export const technologies = [
+  {
+    name: 'HTML5',
+  },
+  {
+    name: 'CSS3',
+  },
+  {
+    name: 'Javascript',
+  },
+  {
+    name: 'Typescript',
+  },
+  {
+    name: 'ReactJS',
+  },
+  {
+    name: 'NextJS',
+  },
+  {
+    name: 'NodeJS',
+  },
+  {
+    name: 'ExpressJS',
+  },
+  {
+    name: 'TailwindCSS',
+  },
+  {
+    name: 'Zustand',
+  },
+  {
+    name: 'Redux',
+  },
+  {
+    name: 'Python',
+  },
+  {
+    name: 'FastAPI',
+  },
+  {
+    name: 'GO',
+  },
+  {
+    name: 'MongoDB',
+  },
+  {
+    name: 'Jira',
+  },
+  {
+    name: 'GitHub',
+  },
 ]
